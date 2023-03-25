d2a1188a4281a014a312bdad81ae4ab15ff59522
Revenant2909 <101965527+Revenant2909@users.noreply.github.com>
2023-03-25 14:55:49 +0530

    UI Complete

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -29,7 +29,7 @@ export default function Register() {
             </p>
             {!email ? (
                 <div className="input">
-            <input type="email" placeholder="email address" ref= {emailRef}/>
+            <input type="email" placeholder=" Email address" ref= {emailRef}/>
             <button className="registerButton"onClick={handleStart}>Get Started </button>
             </div>
             ): (
