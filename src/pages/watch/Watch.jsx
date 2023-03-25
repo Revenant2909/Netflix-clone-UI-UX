113b98e145869458839e4a50dcf8060fda7be21e
Revenant2909 <101965527+Revenant2909@users.noreply.github.com>
2023-03-25 13:37:33 +0530

    Register page
    Register page

--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -1,7 +1,19 @@
+import { ArrowBackOutlined } from "@material-ui/icons"
 import "./watch.scss"
 
 export default function Watch() {
   return (
-    <div>Watch</div>
+    <div className="watch">
+      <div className="back">
+        <ArrowBackOutlined/>
+        Home
+      </div>
+      <video className="video" 
+       autoPlay
+       progress
+       controls 
+      src="
+      "/>
+    </div>
   )
 }
